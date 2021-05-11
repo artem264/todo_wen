0cb46adb35f9015038389ee8967e5fa1416af6b2
Artem Art <>
2021-05-11 15:31:18 +0000

    добавили app.py и прописали бота

---
+++ confg.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "1672778841:AAGB_LZwAu21N08zw3XKfphp_Tj3jFsQ_MU"
+admin_id = 1269490114
+todo = {}
+HELP = """
+add - добавить задачу
+show - показать задачи
+done - убрать выполненную задачу
+help - вывести список команд 
+"""
